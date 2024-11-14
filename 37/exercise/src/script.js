da4a27a754a3713e67ff6ee80f1c3a0b39ca6483
Martijn Benjamin <hello@martijn-benjamin.com>
2024-11-14 15:16:22 +0100

    added lesson 37

--- 37/exercise/src/script.js
+++ 37/exercise/src/script.js
@@ -29,12 +29,14 @@ const sizes = {
     pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
     sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+    // Update materials
+    material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -75,8 +77,7 @@ renderer.setPixelRatio(sizes.pixelRatio)
 
 gui
     .addColor(rendererParameters, 'clearColor')
-    .onChange(() =>
-    {
+    .onChange(() => {
         renderer.setClearColor(rendererParameters.clearColor)
     })
 
@@ -85,6 +86,9 @@ gui
  */
 const materialParameters = {}
 materialParameters.color = '#ff794d'
+materialParameters.shadowColor = '#8e19b8'
+materialParameters.lightColor = '#e5ffe0'
+
 
 const material = new THREE.ShaderMaterial({
     vertexShader: halftoneVertexShader,
@@ -93,15 +97,39 @@ const material = new THREE.ShaderMaterial({
     {
         uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
         uShadeColor: new THREE.Uniform(new THREE.Color(materialParameters.shadeColor)),
+        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+        uShadowRepetitions: new THREE.Uniform(100),
+        uShadowColor: new THREE.Uniform(new THREE.Color(materialParameters.shadowColor)),
+        uLightRepetitions: new THREE.Uniform(100),
+        uLightColor: new THREE.Uniform(new THREE.Color(materialParameters.lightColor)),
     }
 })
 
-gui
-    .addColor(materialParameters, 'color')
-    .onChange(() =>
-    {
+gui.addColor(materialParameters, 'color')
+    .onChange(() => {
         material.uniforms.uColor.value.set(materialParameters.color)
     })
+
+gui.add(material.uniforms.uShadowRepetitions, 'value')
+    .min(1)
+    .max(300)
+    .step(1)
+
+gui.addColor(materialParameters, 'shadowColor')
+    .onChange(() => {
+        material.uniforms.uShadowColor.value.set(materialParameters.shadowColor)
+    })
+
+gui.add(material.uniforms.uLightRepetitions, 'value')
+    .min(1)
+    .max(300)
+    .step(1)
+
+gui.addColor(materialParameters, 'lightColor')
+    .onChange(() => {
+        material.uniforms.uLightColor.value.set(materialParameters.lightColor)
+    })
+
 
 /**
  * Objects
@@ -126,12 +154,10 @@ scene.add(sphere)
 let suzanne = null
 gltfLoader.load(
     './suzanne.glb',
-    (gltf) =>
-    {
+    (gltf) => {
         suzanne = gltf.scene
-        suzanne.traverse((child) =>
-        {
-            if(child.isMesh)
+        suzanne.traverse((child) => {
+            if (child.isMesh)
                 child.material = material
         })
         scene.add(suzanne)
@@ -143,13 +169,11 @@ gltfLoader.load(
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Rotate objects
-    if(suzanne)
-    {
+    if (suzanne) {
         suzanne.rotation.x = - elapsedTime * 0.1
         suzanne.rotation.y = elapsedTime * 0.2
     }
