c9eacc5364a6b7381865caf79a8abb39fb6f8a6a
Martijn Benjamin <hello@martijn-benjamin.com>
2024-11-16 10:53:18 +0100

    Added lesson 38

--- 38/exercise/src/script.js
+++ 38/exercise/src/script.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+
 import earthVertexShader from './shaders/earth/vertex.glsl'
 import earthFragmentShader from './shaders/earth/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphere/vertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphere/fragment.glsl'
 
 /**
  * Base
@@ -22,6 +25,35 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Earth
  */
+const earthParameters = {}
+earthParameters.atmosphereDayColor = '#00aaff'
+earthParameters.atmosphereTwilightColor = '#002aff'
+
+gui.addColor(earthParameters, 'atmosphereDayColor')
+    .onChange(() => {
+        earthMaterial.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor)
+        atmosphereMaterial.uniforms.uAtmosphereDayColor.value.set(earthParameters.atmosphereDayColor)
+
+    })
+
+gui.addColor(earthParameters, 'atmosphereTwilightColor')
+    .onChange(() => {
+        earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor)
+        atmosphereMaterial.uniforms.uAtmosphereTwilightColor.value.set(earthParameters.atmosphereTwilightColor)
+    })
+
+// Textures
+const earthDayTexture = textureLoader.load('./earth/day.jpg')
+earthDayTexture.colorSpace = THREE.SRGBColorSpace
+earthDayTexture.anisotropy = 8
+
+const earthNightTexture = textureLoader.load('./earth/night.jpg')
+earthNightTexture.colorSpace = THREE.SRGBColorSpace
+earthNightTexture.anisotropy = 8
+
+const earthSpecularCloudsTexture = textureLoader.load('./earth/specularClouds.jpg')
+earthSpecularCloudsTexture.anisotropy = 8
+
 // Mesh
 const earthGeometry = new THREE.SphereGeometry(2, 64, 64)
 const earthMaterial = new THREE.ShaderMaterial({
@@ -29,10 +61,67 @@ const earthMaterial = new THREE.ShaderMaterial({
     fragmentShader: earthFragmentShader,
     uniforms:
     {
+        uDayTexture: new THREE.Uniform(earthDayTexture),
+        uNightTexture: new THREE.Uniform(earthNightTexture),
+        uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture),
+        uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+        uAtmosphereDayColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereDayColor)),
+        uAtmosphereTwilightColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereTwilightColor))
     }
 })
 const earth = new THREE.Mesh(earthGeometry, earthMaterial)
 scene.add(earth)
+
+// Atmosphere
+const atmosphereMaterial = new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    side: THREE.BackSide,
+    transparent: true,
+    uniforms:
+    {
+        uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
+        uAtmosphereDayColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereDayColor)),
+        uAtmosphereTwilightColor: new THREE.Uniform(new THREE.Color(earthParameters.atmosphereTwilightColor))
+    }
+})
+const atmosphere = new THREE.Mesh(earthGeometry, atmosphereMaterial)
+atmosphere.scale.set(1.03, 1.03, 1.03)
+scene.add(atmosphere)
+
+/**
+ * Sun
+ */
+const sunSpeherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5)
+const sunDirection = new THREE.Vector3()
+
+// debug
+const debugSun = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(0.1, 2),
+    new THREE.MeshBasicMaterial()
+)
+scene.add(debugSun)
+
+// update
+const updateSun = () => {
+
+    // sun direction
+    sunDirection.setFromSpherical(sunSpeherical)
+
+    // debug
+    debugSun.position.copy(sunDirection).multiplyScalar(10)
+
+    // Unfirms
+    earthMaterial.uniforms.uSunDirection.value.copy(sunDirection)
+    atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection)
+
+}
+updateSun()
+
+// Tweaks
+gui.add(sunSpeherical, 'phi').min(0).max(Math.PI).onChange(updateSun)
+gui.add(sunSpeherical, 'theta').min(-Math.PI).max(Math.PI).onChange(updateSun)
+
 
 /**
  * Sizes
@@ -43,8 +132,8 @@ const sizes = {
     pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
+
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -64,9 +153,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 12
-camera.position.y = 5
-camera.position.z = 4
+camera.position.x = 9
+camera.position.y = 2 // up and down
+camera.position.z = -3
 scene.add(camera)
 
 // Controls
@@ -89,8 +178,7 @@ renderer.setClearColor('#000011')
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     earth.rotation.y = elapsedTime * 0.1
--- 38/exercise/src/shaders/earth/fragment.glsl
+++ 38/exercise/src/shaders/earth/fragment.glsl
@@ -1,12 +1,46 @@
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+uniform vec3 uSunDirection;
+uniform vec3 uAtmosphereDayColor;
+uniform vec3 uAtmosphereTwilightColor;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
-void main()
-{
+void main() {
+
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // sun orientation
+    float sunOrientation = dot(uSunDirection, normal);
+    color = vec3(sunOrientation);
+
+    // Day / night color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
+
+    // specular cloud color
+    vec2 specularCloudColor = texture(uSpecularCloudsTexture, vUv).rg;
+
+    // Clouds
+    float cloudsMix = smoothstep(0.5, 1.0, specularCloudColor.g);
+    cloudsMix *= dayMix;
+    color = mix(color, vec3(1.0), cloudsMix);
+
+    // fresnel
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 2.0);
+
+    // Atmosphere
+    float atmosphereDayMix = smoothstep(-0.5, 1.0, sunOrientation);
+    vec3 atmosphereColor = mix(uAtmosphereTwilightColor, uAtmosphereDayColor, atmosphereDayMix);
+    color = mix(color, atmosphereColor, fresnel * atmosphereDayMix);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
