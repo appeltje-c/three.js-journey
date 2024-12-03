1ef713eaf53fc7995beab529678d4dadc41b20f2
Martijn Benjamin <hello@martijn-benjamin.com>
2024-12-03 09:02:24 +0100

    finished lesson 57

--- 57/exercise/src/Experience.jsx
+++ 57/exercise/src/Experience.jsx
@@ -1,25 +1,47 @@
 import { OrbitControls } from '@react-three/drei'
+import { button, useControls } from 'leva'
+import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
+
+    const { position, color, visible, scale } = useControls('Cube', {
+        position: {
+            value: { x: -2, y: 0 },
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: 'mediumpurple',
+        visible: true,
+        myInterval: {
+            min: 0,
+            max: 10,
+            value: [4, 5]
+        },
+        clickMe: button(() => { console.log('click') }),
+        choice: { options: ['one', 'two', 'three'] },
+        scale: { value: 1, min: 0, max: 5, step: 0.01 }
+    })
+
     return <>
+
+        <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position={[position.x, position.y, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh position-x={2} scale={scale} visible={visible}>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial color={color} />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
